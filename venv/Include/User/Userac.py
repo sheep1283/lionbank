16806a1ed4d0527c0102159aa4312f7a0e04ea94
bell <hwayeon3550@naver.com>
2020-05-07 01:42:32 +0900

    입출금class

--- venv/Include/User/Userac.py
+++ venv/Include/User/Userac.py
@@ -1,16 +1,16 @@
 class Userac:
-    def __init__(self, name, account, deposit):
+    def __init__(self, name, account, balance):
         self.name = name
         self.account = account
-        self.deposit = int(deposit)
+        self.balance = int(balance)
 
 
     def print_info(self):
-        print("계좌번호 : ", self.account, "/ 이름 : ", self.name, " / 잔액 : ", self.deposit, "원")
+        print("계좌번호 : ", self.account, "/ 이름 : ", self.name, " / 잔액 : ", self.balance, "원")
     
     def user_account(self):
         return self.account
     def print_name(self):
         print("계좌이름 : ", self.name)
-    def print_deposit(self):
-        print("계좌잔고 : ", self.deposit)
+    def print_balance(self):
+        print("계좌잔고 : ", self.balance)
--- venv/Include/UserAccount.py
+++ venv/Include/UserAccount.py
@@ -27,20 +27,24 @@ if __name__=="__main__":
             for i in range(len(userlist)) :
                 if userlist[i].account == accnum :
                     ind = i
-            userlist[ind].print_deposit()
+            userlist[ind].print_balance()
             userlist[ind].print_name()
             money1 = int(input("입금하실 금액을 입력해주세요 : "))
-            userlist[ind].deposit = userlist[ind].deposit + money1
-            print("##계좌잔고 : %d 원##" %userlist[ind].deposit)
+            userlist[ind].balance = userlist[ind].balance + money1
+            print("##계좌잔고 : %d 원##" %userlist[ind].balance)
             print("##입금이 완료되었습니다##\n", "====================")
 
         elif (num == 3):
             print("======출금하기======")
             accnum = input("출금하실 계좌번호를 입력해주세요 : ")
-            print("계좌이름 : {0}\n계좌잔고 : {1}" .format(name, depo))
-            money2 = input("출금하실 금액을 입력해주세요 : ")
-            depo -= money2
-            print("##계좌잔고 : %d 원##" %depo)
+            for i in range(len(userlist)) :
+                if userlist[i].account == accnum :
+                    ind = i
+            userlist[ind].print_balance()
+            userlist[ind].print_name()
+            money2 = int(input("출금하실 금액을 입력해주세요 : "))
+            userlist[ind].balance = userlist[ind].balance - money2
+            print("##계좌잔고 : %d 원##" %userlist[ind].balance)
             print("##출금이 완료되었습니다##\n", "====================")
 
         elif(num == 4):
