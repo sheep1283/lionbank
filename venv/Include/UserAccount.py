63bead73b8d2a178a6396e95831ebfc3750ed09f
bell <hwayeon3550@naver.com>
2020-05-07 00:33:29 +0900

    입출금

--- venv/Include/UserAccount.py
+++ venv/Include/UserAccount.py
@@ -13,17 +13,32 @@ if __name__=="__main__":
         if(num == 1):
             print("======계좌개설======")
             accnum = input("계좌번호 : ")
-            name = input("이름 : "),...
+            name = input("이름 : ")
             depo = input("예금 : ")
             #입력받은 계좌, 이름, 예금 정보를 User.Userac 클래스에 입력하고, 입력 정보를 accnum에 저장(여기서 accnum의 type은 char에서 해당 클래스의 type으로 바뀜)
             accnum = user.Userac(name, accnum, depo)
             #userlist에 생성된 유저 정보 저장(이름, 계좌, 예금)
             userlist.append(accnum)
             print("##계좌개설을 완료하였습니다##\n", "==================")
-        elif(num == 2):
-            print("2")
-        elif(num == 3):
-            print("3")
+
+        elif (num == 2):
+            print("======입금하기======")
+            accnum = input("입금하실 계좌번호를 입력해주세요 : ")
+            print("계좌이름 : {0}\n계좌잔고 : {1}" .format(name, depo))
+            money1 = input("입금하실 금액을 입력해주세요 : ")
+            depo += money1
+            print("##계좌잔고 : %d 원##" %depo)
+            print("##입금이 완료되었습니다##\n", "====================")
+
+        elif (num == 3):
+            print("======출금하기======")
+            accnum = input("출금하실 계좌번호를 입력해주세요 : ")
+            print("계좌이름 : {0}\n계좌잔고 : {1}" .format(name, depo))
+            money2 = input("출금하실 금액을 입력해주세요 : ")
+            depo -= money2
+            print("##계좌잔고 : %d 원##" %depo)
+            print("##출금이 완료되었습니다##\n", "====================")
+
         elif(num == 4):
             #userlist의 크기(저장된 사람수)로 for문 돌려 0번째부터 userlist의 크기번째까지 모두 출력
             for i in range(len(userlist)):
